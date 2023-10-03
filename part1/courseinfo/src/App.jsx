6fc25e6a90cf1c130199374469dfeed412b8fb37
yogeshwaran <yogesh.ktm@gmail.com>
2023-10-03 08:15:37 +0530

    Course info: Step 4 completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,25 +5,27 @@ import { Total } from './components/Total'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
 
   return (
     <div>
       <Header course = {course}></Header>
-      <Content part1={part1.name} part2={part2.name} part3={part3.name} exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}></Content>
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}></Total>
+      <Content parts={parts}></Content>
+      <Total parts={parts} exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises}></Total>
     </div>
   )
 }
--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -3,9 +3,9 @@ import { Part } from './Part';
 export const Content = (props) => {
     return(
        <>
-       <Part partno={props.part1} partexcersizes={props.exercises1}></Part>
-       <Part partno={props.part2} partexcersizes={props.exercises2}></Part>
-       <Part partno={props.part3} partexcersizes={props.exercises3}></Part>
+       <Part partno={props.parts[0].name} partexcersizes={props.parts[0].excersizes}></Part>
+       <Part partno={props.parts[1].name} partexcersizes={props.parts[1].excersizes}></Part>
+       <Part partno={props.parts[2].name} partexcersizes={props.parts[2].excersizes}></Part>
        </>
     )
 }
