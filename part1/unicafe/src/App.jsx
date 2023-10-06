b2aa6a54710dd93d6e6b5c38ce47a6235124dc45
yogeshwaran <yogesh.ktm@gmail.com>
2023-10-06 08:47:12 +0530

    Part 1 : Unicafe step 1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <div>
+        code here
+      </div>
+    </>
+  )
+}
+
+export default App
